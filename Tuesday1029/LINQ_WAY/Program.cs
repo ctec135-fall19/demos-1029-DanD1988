254158ef91e78b8dcfae668a196c4d90043d5a40
Daniel Davidson <DanDavidson88@gmail.com>
2019-10-29 15:06:09 -0700

    This was a long demo

---
+++ Tuesday1029/LINQ_WAY/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_WAY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+
+            string[] currentVideoGames = { "Morrowind", "Uncharted 2", "Fallout 3", "Dexter", "System Shock 2" };
+
+            QueryOverString(currentVideoGames);
+
+            #endregion
+
+            
+        }
+        #region
+
+        static void QueryOverString(string[] inputArray)
+        {
+            // build querry
+            // IEnumerable<string> subset = from ...
+            var subset = from game in inputArray
+                         where game.Contains(" ")
+                         orderby game
+                         select game;
+
+            // print results
+            ReflectOverQueryResults(subset, "Querry Expression");
+
+            // print results 
+            Console.WriteLine("  Immediate results using LINQ query");
+            foreach (var s in subset)
+            {
+                Console.WriteLine("Item: {0}", s);
+            }
+            Console.WriteLine();
+
+            // demnstrate reuse of query 
+            // example of deffered execution
+            inputArray[0] = "some string";
+            Console.WriteLine("  Immediate results using LINQ query");
+            foreach (var s in subset)
+            {
+                Console.WriteLine("Item: {0}", s);
+            }
+            Console.WriteLine();
+
+            // demonstrate returning result - immediate execution 
+            List<string> outputList = (from game in inputArray
+                                       where game.Contains(" ")
+                                       orderby game
+                                       select game).ToList<string>();
+            return outputList;
+
+        }
+
+        static void ReflectOverQueryResults(object resultSet, string queryType)
+        {
+            Console.WriteLine("*** query type: {0}", queryType);
+            Console.WriteLine("resultSet is of type: {0}", resultSet.GetType().Name);
+            Console.WriteLine("resultSet location", resultSet.GetType().Assembly.GetName().Name);
+        }
+
+        #endregion
+    }
+}
---
+++ Tuesday1029/LinqToArray/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqToArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Fun with LINQ to objects ****\n");
+
+            // define an array of strings
+
+            // This is the input array
+            string[] currentVideoGames = { "Morrowind", "Uncharted 2", "Fallout 3", "Dexter", "System Shock 2" };
+
+            // desired query: Games that have a space in the title
+
+            #region First let's try it the olde fashioned way 
+            string[] result = QueryOverStringLongHand(currentVideoGames);
+
+            #endregion
+
+        }
+
+        #region
+        // This is the result array
+        // creates an array of strings. Nothing in it...
+        // If it has space puts it in array
+        static void QueryOverStringLongHand(string[] s)
+        {
+            // temp array
+            string[] resultsWithSpaces = new string[s.Length];
+
+            // find the results 
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i].Contains(" "))
+                    resultsWithSpaces[i] = s[i];
+            }
+
+            // sort results
+            Array.Sort(resultsWithSpaces);
+
+            // print results
+
+            Console.WriteLine("Immediate results from longhand version.");
+            foreach (string s1 in resultsWithSpaces)
+            {
+                if (s1 != null)
+                    Console.WriteLine("Item: {0}", s1);
+
+            }
+            Console.WriteLine();
+
+            // Generate a return array
+            // Figure out size
+
+            int count = 0;
+            foreach (string s2 in resultsWithSpaces)
+            {
+                if (s2 != null) count++;
+            }
+
+            // create output array
+            string[] outPutArray = new string[count];
+
+            // Populate output array
+            count = 0;
+            foreach (string s1 in resultsWithSpaces)
+            {
+                if (s1 != null)
+                {
+                    outPutArray[count] = s1;
+                    count++;
+                }
+            }
+
+            return outPutArray;
+
+        }
+
+        #endregion
+
+
+    }
+}
